5b3c588a8e8505722cfba1aa574b6b708a595553
Paris Rainey <paris.c.rainey@gmail.com>
2023-08-23 15:05:57 -0500

    Began Initializing Game Stats and Added Intro

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -10,6 +10,39 @@ namespace HelloDungeon
     {
         public void Run()
         {
+            //Initializes Game Stats
+            string playerName = "Traveler";
+            string enemy1 = "Lagoona";
+            string enemy2 = "Frostbite";
+            string enemy3 = "Gargon";
+            int maxEnemies = 0;
+            float health = 100;
+            bool playerIsAlive = true;
+            float damage = 1.1f;
+            float strength = 1.0f;
+            float defense = 0.0f; 
+            int dungeonNumber = 0;
+            playerIsAlive = health > 0;
+            bool gameOver = health < 0;
+
+            //Introduction to Player
+            Console.WriteLine("Gillian Welcomes You");
+            Console.WriteLine("What shall I call you " + playerName + "?");
+
+            //Establish Player Name
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            playerName = firstName + lastName;
+            Console.WriteLine("Oh, " + playerName + "? I believe that was on the board back in town.");
+
+            //Introduction to the Game
+            Console.WriteLine("Regardless, Welcome to the cave of Lusmila on the island of Juda.");
+            Console.WriteLine("The reason you're here," + playerName + "is to excavate the legendary treasure of Juda.");
+            Console.WriteLine("This treasure has been sought after by many adventurers before you.");
+
+            
+            
+
 
         }
     }
